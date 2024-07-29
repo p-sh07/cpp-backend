f6c11ada65f45af81d4174838a3411c43d12130e
psh <p.sh07@ya.ru>
2024-07-29 14:07:18 +0300

    multi-thread start

--- sprint1/problems/restaurant_multi_threaded/solution/src/main.cpp
+++ sprint1/problems/restaurant_multi_threaded/solution/src/main.cpp
@@ -5,11 +5,10 @@
 #include <boost/asio.hpp>
 #include <chrono>
 #include <iostream>
+#include <memory>
 #include <mutex>
 #include <sstream>
 //#include <syncstream>
-#include <unordered_map>
-#include <memory>
 #include <thread>
 
 namespace as = boost::asio;
@@ -17,57 +16,59 @@ namespace sys = boost::system;
 namespace ph = std::placeholders;
 
 using namespace std::chrono;
-
 using namespace std::literals;
+
 using Timer = as::steady_timer;
 
-class Hamburger {
-public:
-    [[nodiscard]] bool IsBurgerRoasted() const {
-        return burger_roasted_;
-    }
-    void SetBurgerRoasted() {
-        if (IsBurgerRoasted()) {  // Котлету можно жарить только один раз
-            throw std::logic_error("Burger has been roasted already"s);
+namespace {
+
+    class Hamburger {
+    public:
+        [[nodiscard]] bool IsBurgerRoasted() const {
+            return burger_roasted_;
         }
-        burger_roasted_ = true;
-    }
-
-    [[nodiscard]] bool HasOnion() const {
-        return has_onion_;
-    }
-    // Добавляем лук
-    void AddOnion() {
-        if (IsPacked()) {  // Если гамбургер упакован, класть лук в него нельзя
-            throw std::logic_error("Hamburger has been packed already"s);
+        void SetBurgerRoasted() {
+            if (IsBurgerRoasted()) {  // Котлету можно жарить только один раз
+                throw std::logic_error("Burger has been roasted already"s);
+            }
+            burger_roasted_ = true;
         }
-        AssureBurgerRoasted();  // Лук разрешается класть лишь после прожаривания котлеты
-        has_onion_ = true;
-    }
 
-    [[nodiscard]] bool IsPacked() const {
-        return is_packed_;
-    }
-    void Pack() {
-        AssureBurgerRoasted();  // Нельзя упаковывать гамбургер, если котлета не прожарена
-        is_packed_ = true;
-    }
-
-private:
-    // Убеждаемся, что котлета прожарена
-    void AssureBurgerRoasted() const {
-        if (!burger_roasted_) {
-            throw std::logic_error("Bread has not been roasted yet"s);
+        [[nodiscard]] bool HasOnion() const {
+            return has_onion_;
         }
-    }
+        // Добавляем лук
+        void AddOnion() {
+            if (IsPacked()) {  // Если гамбургер упакован, класть лук в него нельзя
+                throw std::logic_error("Hamburger has been packed already"s);
+            }
+            AssureBurgerRoasted();  // Лук разрешается класть лишь после прожаривания котлеты
+            has_onion_ = true;
+        }
 
-    bool burger_roasted_ = false;  // Обжарена ли котлета?
-    bool has_onion_ = false;       // Есть ли лук?
-    bool is_packed_ = false;       // Упакован ли гамбургер?
-};
+        [[nodiscard]] bool IsPacked() const {
+            return is_packed_;
+        }
+        void Pack() {
+            AssureBurgerRoasted();  // Нельзя упаковывать гамбургер, если котлета не прожарена
+            is_packed_ = true;
+        }
+
+    private:
+        // Убеждаемся, что котлета прожарена
+        void AssureBurgerRoasted() const {
+            if (!burger_roasted_) {
+                throw std::logic_error("Bread has not been roasted yet"s);
+            }
+        }
+
+        bool burger_roasted_ = false;  // Обжарена ли котлета?
+        bool has_onion_ = false;       // Есть ли лук?
+        bool is_packed_ = false;       // Упакован ли гамбургер?
+    };
 
 std::ostream& operator<<(std::ostream& os, const Hamburger& h) {
-    return os << "Hamburger: "sv << (h.IsBurgerRoasted() ? "roasted burger"sv : " raw burger"sv)
+    return os << "Hamburger: "sv << (h.IsCutletRoasted() ? "roasted cutlet"sv : " raw cutlet"sv)
               << (h.HasOnion() ? ", onion"sv : ""sv)
               << (h.IsPacked() ? ", packed"sv : ", not packed"sv);
 }
@@ -75,13 +76,13 @@ std::ostream& operator<<(std::ostream& os, const Hamburger& h) {
 class Logger {
 public:
     explicit Logger(std::string id)
-        : id_(std::move(id)) {
+            : id_(std::move(id)) {
     }
 
     void LogMessage(std::string_view message) const {
         //std::osyncstream os{std::cout};
         std::cout << id_ << "> ["sv << duration<double>(steady_clock::now() - start_time_).count()
-           << "s] "sv << message << std::endl;
+                  << "s] "sv << message << std::endl;
     }
 
 private:
@@ -89,32 +90,50 @@ private:
     steady_clock::time_point start_time_{steady_clock::now()};
 };
 
+class ThreadChecker {
+public:
+    explicit ThreadChecker(std::atomic_int& counter)
+        : counter_{counter} {
+    }
+
+    ThreadChecker(const ThreadChecker&) = delete;
+    ThreadChecker& operator=(const ThreadChecker&) = delete;
+
+    ~ThreadChecker() {
+        // assert выстрелит, если между вызовом конструктора и деструктора
+        // значение expected_counter_ изменится
+        assert(expected_counter_ == counter_);
+    }
+
+private:
+    std::atomic_int& counter_;
+    int expected_counter_ = ++counter_;
+};
+
 // Функция, которая будет вызвана по окончании обработки заказа
 using OrderHandler = std::function<void(sys::error_code ec, int id, Hamburger* hamburger)>;
 
 class Order : public std::enable_shared_from_this<Order> {
 public:
-    Order(as::io_context& io, int id, bool with_onion, OrderHandler handler)
-            : io_(io)
-            , id_(id)
-            , with_onion_(with_onion)
-            , handler_(std::move(handler)) {
+    Order(as::io_context &io, int id, bool with_onion, OrderHandler handler)
+            : io_(io), id_(id), with_onion_(with_onion), handler_(std::move(handler)) {
     }
 
     // Запускает асинхронное выполнение заказа
     void Execute() {
         logger_.LogMessage("Order has been started."sv);
         RoastBurger();
-        if(with_onion_) {
+        if (with_onion_) {
             MarinateOnion();
         }
     }
+
 private:
-    as::io_context& io_;
+    as::io_context &io_;
     int id_;
 
     bool with_onion_ = false, onion_done_ = false,
-    delivered_ = false;
+            delivered_ = false;
 
     OrderHandler handler_;
 
@@ -218,6 +237,7 @@ private:
     }
 };
 
+
 class Restaurant {
 public:
     explicit Restaurant(as::io_context& io)
@@ -230,6 +250,7 @@ public:
         std::make_shared<Order>(io_, order_id, with_onion, std::move(handler))->Execute();
 
         return order_id;
+
     }
 
 private:
@@ -237,8 +258,26 @@ private:
     int next_order_id_ = 0;
 };
 
+
+
+// Запускает функцию fn на n потоках, включая текущий
+    template <typename Fn>
+    void RunWorkers(unsigned n, const Fn& fn) {
+        n = std::max(1u, n);
+        std::vector<std::jthread> workers;
+        workers.reserve(n - 1);
+        // Запускаем n-1 рабочих потоков, выполняющих функцию fn
+        while (--n) {
+            workers.emplace_back(fn);
+        }
+        fn();
+    }
+
+}  // namespace
+
 int main() {
-    as::io_context io;
+    const unsigned num_workers = 4;
+    as::io_context io(num_workers);
 
     Restaurant restaurant{io};
 
@@ -250,33 +289,32 @@ int main() {
     };
 
     std::unordered_map<int, OrderResult> orders;
-    auto handle_result = [&orders](sys::error_code ec, int id, Hamburger* h) {
-        orders.emplace(id, OrderResult{ec, ec ? Hamburger{} : *h});
-    };
 
-    const int id1 = restaurant.MakeHamburger(false, handle_result);
-    const int id2 = restaurant.MakeHamburger(true, handle_result);
+    // Обработчик заказа может быть вызван в любом из потоков, вызывающих io.run().
+    // Чтобы избежать состояния гонки при обращении к orders, выполняем обращения к orders через
+    // strand, используя функцию dispatch.
+    auto handle_result
+            = [strand = as::make_strand(io), &orders](sys::error_code ec, int id, Hamburger* h) {
+                as::dispatch(strand, [&orders, id, res = OrderResult{ec, ec ? Hamburger{} : *h}] {
+                    orders.emplace(id, res);
+                });
+            };
 
-    // До вызова io.run() никакие заказы не выполняются
-    assert(orders.empty());
-    io.run();
-
-    // После вызова io.run() все заказы быть выполнены
-    assert(orders.size() == 2u);
-    {
-        // Проверяем заказ без лука
-        const auto& o = orders.at(id1);
-        assert(!o.ec);
-        assert(o.hamburger.IsBurgerRoasted());
-        assert(o.hamburger.IsPacked());
-        assert(!o.hamburger.HasOnion());
+    const int num_orders = 16;
+    for (int i = 0; i < num_orders; ++i) {
+        restaurant.MakeHamburger(i % 2 == 0, handle_result);
     }
-    {
-        // Проверяем заказ с луком
-        const auto& o = orders.at(id2);
-        assert(!o.ec);
-        assert(o.hamburger.IsBurgerRoasted());
-        assert(o.hamburger.IsPacked());
-        assert(o.hamburger.HasOnion());
+
+    assert(orders.empty());
+    RunWorkers(num_workers, [&io] {
+        io.run();
+    });
+    assert(orders.size() == num_orders);
+
+    for (const auto& [id, order] : orders) {
+        assert(!order.ec);
+        assert(order.hamburger.IsBurgerRoasted());
+        assert(order.hamburger.IsPacked());
+        assert(order.hamburger.HasOnion() == (id % 2 != 0));
     }
 }
