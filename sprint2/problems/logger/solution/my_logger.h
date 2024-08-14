40c92c483ba967bde582acc2c76f8d53f98b806f
psh <p.sh07@ya.ru>
2024-08-14 22:06:21 +0300

    logger start

---
+++ sprint2/problems/logger/solution/my_logger.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <optional>
+#include <mutex>
+#include <thread>
+
+using namespace std::literals;
+
+#define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
+
+class Logger {
+    auto GetTime() const {
+        if (manual_ts_) {
+            return *manual_ts_;
+        }
+
+        return std::chrono::system_clock::now();
+    }
+
+    auto GetTimeStamp() const {
+        const auto now = GetTime();
+        const auto t_c = std::chrono::system_clock::to_time_t(now);
+        return std::put_time(std::localtime(&t_c), "%F %T");
+    }
+
+    // Для имени файла возьмите дату с форматом "%Y_%m_%d"
+    std::string GetFileTimeStamp() const {
+        const auto now = GetTime();
+        return std::
+    }
+
+    Logger() = default;
+    Logger(const Logger&) = delete;
+
+public:
+    static Logger& GetInstance() {
+        static Logger obj;
+        return obj;
+    }
+
+    // Выведите в поток все аргументы.
+    template<class... Ts>
+    void Log(const Ts&... args) {
+        std::lock_guard(m_);
+
+        log_file_ << GetTimeStamp() << ": "sv ;
+        cb(log_file_);
+        log_file_ << std::endl;
+    }
+
+    // Установите manual_ts_. Учтите, что эта операция может выполняться
+    // параллельно с выводом в поток, вам нужно предусмотреть 
+    // синхронизацию.
+    void SetTimestamp(std::chrono::system_clock::time_point ts) {
+        std::lock_guard(m_);
+        manual_ts_.emplace(ts);
+    }
+
+private:
+    std::mutex m_;
+    std::optional<std::chrono::system_clock::time_point> manual_ts_;
+
+    // для демонстрации пока оставим файл в текущей директории
+    std::ofstream log_file_{"sample.log"s};
+};
