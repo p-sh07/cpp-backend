c3a8c92d3af0da8ce050d91d72c11916b28184c0
p.sh07 <ps@mbp.com>
2024-08-16 12:13:41 +0300

    example

---
+++ sprint2/examples/boost_log/main.cpp
@@ -0,0 +1,14 @@
+#include <boost/log/trivial.hpp>
+
+#include <string_view>
+
+using namespace std::literals;
+
+int main() {
+    BOOST_LOG_TRIVIAL(trace) << "Сообщение уровня trace"sv;
+    BOOST_LOG_TRIVIAL(debug) << "Сообщение уровня debug"sv;
+    BOOST_LOG_TRIVIAL(info) << "Сообщение уровня info"sv;
+    BOOST_LOG_TRIVIAL(warning) << "Сообщение уровня warning"sv;
+    BOOST_LOG_TRIVIAL(error) << "Сообщение уровня error"sv;
+    BOOST_LOG_TRIVIAL(fatal) << "Сообщение уровня fatal"sv;
+}
